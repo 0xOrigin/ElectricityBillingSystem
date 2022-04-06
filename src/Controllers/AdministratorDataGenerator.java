65a048b12ba9758cad4730aa410e263dc2b90535
walid404 <walid.max28@gmail.com>
2022-04-06 17:09:52 +0200

    Implementation of DataGenerators

--- src/Controllers/AdministratorDataGenerator.java
+++ src/Controllers/AdministratorDataGenerator.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+
 /**
  *
  * @author xorigin
@@ -7,11 +8,33 @@ package Controllers;
 public class AdministratorDataGenerator extends DataGenerator{
     
     String generatePassword(){
-        throw new UnsupportedOperationException("Not implemented yet.");
+        
+        final String AVAILABLECHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-";
+        System.out.println(AVAILABLECHARS.length());
+        final int PASSWORDLENGTH = 10;
+        String password = "";
+        for (int counter = 0; counter < PASSWORDLENGTH; counter++) {
+            int randomNumber = generateRandomNumber(AVAILABLECHARS.length());
+            char randomChar = getRandomChar(AVAILABLECHARS , randomNumber);
+            password += randomChar ;
+        }
+        return password;
+                
+    }
+    private char getRandomChar(String availableChars,int randomNumber){
+        
+        char randomChar = availableChars.charAt(randomNumber);
+        return randomChar ;
+        
     }
     
-    String generateID(){
-        throw new UnsupportedOperationException("Not implemented yet.");
+    
+    String generateID(String nationalID){
+        
+        String ID = generateRandomNumbers(2) + nationalID.substring(7, 9) + generateRandomNumbers(4) + nationalID.substring(12, 14);
+        return ID;
+        
     }
+    
     
 }
