5eb9dc1fe234c620e5493e805f2036d7a94e7424
~ Son Of Bitar ~ <73714574+walid404@users.noreply.github.com>
2022-04-06 04:09:06 +0200

    implement CustomerDataGenerator

--- src/Controllers/CustomerDataGenerator.java
+++ src/Controllers/CustomerDataGenerator.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+
 /**
  *
  * @author xorigin
@@ -7,11 +8,50 @@ package Controllers;
 public class CustomerDataGenerator extends DataGenerator{
     
     String generateMeterCode(String governmentCode, String nationalID, int numberOfRegisteredCustomers){
-        throw new UnsupportedOperationException("Not implemented yet.");
+         
+        String meterCode = governmentCode + generateRandomNumbers(3) + nationalID.substring(7, 9)
+                + generateEightNumbers("" + (numberOfRegisteredCustomers + 1));
+        return meterCode;
+        
     }
     
-    String generatePassword(){
-        throw new UnsupportedOperationException("Not implemented yet.");
+    String generateEightNumbers(String number){
+        
+        String eightNumber ="";
+        int numberOfZeros = 8 - number.length();
+        
+        while(eightNumber.length()<numberOfZeros){
+            eightNumber += "0";
+        }
+        
+        eightNumber += number;
+        return eightNumber;
+        
     }
+    
+    
+    String generatePassword(){
+        
+        final String AVAILABLECHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-";
+        System.out.println(AVAILABLECHARS.length());
+        final int PASSWORDLENGTH = 10;
+        String password = "";
+        
+        for (int counter = 0; counter < PASSWORDLENGTH; counter++) {
+            int randomNumber = generateRandomNumber(AVAILABLECHARS.length());
+            char randomChar = generateRandomChar(AVAILABLECHARS , randomNumber);
+            password += randomChar ;
+        }
+        return password;
+                
+    }
+    
+    private char generateRandomChar(String availableChars,int randomNumber){
+        
+        char randomChar = availableChars.charAt(randomNumber);
+        return randomChar ;
+        
+    }
+    
     
 }
